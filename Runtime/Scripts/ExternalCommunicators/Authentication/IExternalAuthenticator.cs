3bf4f4b8c3a9964ef8ec32eca53f1b4ab4eee19a
Krzysztof Pięta <krzysztof.pieta@elympics.cc>
2025-01-09 17:09:21 +0100

    feat: add region update
    mr: mr fixes

--- Runtime/Scripts/ExternalCommunicators/Authentication/IExternalAuthenticator.cs
+++ Runtime/Scripts/ExternalCommunicators/Authentication/IExternalAuthenticator.cs
@@ -4,11 +4,13 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using Elympics.Models.Authentication;
 using ElympicsPlayPad.ExternalCommunicators.Authentication.Models;
+using NewRegionName = System.String;
 
 namespace ElympicsPlayPad.ExternalCommunicators.Authentication
 {
     public interface IExternalAuthenticator
     {
+        public event Action<NewRegionName>? RegionUpdated;
         public event Action<AuthData>? AuthenticationUpdated;
         public UniTask<AuthData> Authenticate(CancellationToken ct = default);
         public UniTask<HandshakeInfo> InitializationMessage(
