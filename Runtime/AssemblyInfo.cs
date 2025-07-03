f3da4da91a2ab85f3ce65c95ab5aa5fa3dcf4aa7
Kacper Dudziak <kacper.dudziak@elympics.ai>
2025-07-03 16:42:50 +0200

    chore: bump version to 1.6.1

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("Elympics")]
 [assembly: AssemblyProduct("Elympics PlayPad")]
-[assembly: AssemblyVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.6.1.0")]
 [assembly: InternalsVisibleTo("ElympicsPlayPad.Tests.PlayMode")]
 [assembly: InternalsVisibleTo("ElympicsPlayPad.Editor")]
 
