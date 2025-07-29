c2208ab3acfa83a2cf06318db3d975a0d6d670b5
Kacper Dudziak <kacper.dudziak@elympics.ai>
2025-07-29 16:39:49 +0200

    refactor: return empty collections instead of null
    This change improves the public API by avoiding null references

--- Runtime/Scripts/ExternalCommunicators/VirtualDeposit/IExternalBlockChainCurrencyCommunicator.cs
+++ Runtime/Scripts/ExternalCommunicators/VirtualDeposit/IExternalBlockChainCurrencyCommunicator.cs
@@ -12,14 +12,14 @@ namespace ElympicsPlayPad.ExternalCommunicators.VirtualDeposit
     public interface IExternalBlockChainCurrencyCommunicator
     {
         [PublicAPI]
-        IReadOnlyDictionary<Guid, CoinInfo>? ElympicsCoins { get; }
+        IReadOnlyDictionary<Guid, CoinInfo> ElympicsCoins { get; }
 
         /// <summary>
         /// All currently available battle wallets mapped to coins they store using coin IDs.
         /// The availability of specific wallets is determined by the game configuration, current platform and wallets that a user has connected to their account.
         /// </summary>
         [PublicAPI]
-        IReadOnlyDictionary<Guid, VirtualDepositInfo>? UserDepositCollection { get; }
+        IReadOnlyDictionary<Guid, VirtualDepositInfo> UserDepositCollection { get; }
 
         [PublicAPI]
         event Action<VirtualDepositInfo>? VirtualDepositUpdated;
@@ -31,10 +31,10 @@ namespace ElympicsPlayPad.ExternalCommunicators.VirtualDeposit
         /// <param name="coinId">ID of the coin for which battle wallet should be used in the window.</param>
         [PublicAPI]
         UniTask DisplayDepositPopup(Guid coinId, CancellationToken ct = default);
-        UniTask<IReadOnlyDictionary<Guid, VirtualDepositInfo>?> GetVirtualDeposit(CancellationToken ct = default);
+        UniTask<IReadOnlyDictionary<Guid, VirtualDepositInfo>> GetVirtualDeposit(CancellationToken ct = default);
         [Obsolete("This method no longer needs to be called and will be removed in the future. The process of ensuring proper deposit values is now handled internally by the SDK.")]
         UniTask<EnsureDepositInfo> EnsureVirtualDeposit(decimal amount, CoinInfo coinInfo, CancellationToken ct = default) => UniTask.FromResult(new EnsureDepositInfo { Success = true });
-        UniTask<IReadOnlyDictionary<Guid, CoinInfo>?> GetElympicsCoins(CancellationToken ct = default);
+        UniTask<IReadOnlyDictionary<Guid, CoinInfo>> GetElympicsCoins(CancellationToken ct = default);
 
         UniTask<WalletBalanceInfo> GetConnectedWalletCurrencyBalance(Guid coinId, CancellationToken ct = default);
 
