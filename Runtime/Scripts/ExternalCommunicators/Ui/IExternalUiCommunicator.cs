28edab775f6fc7a30052923b4b8013c4f1dd3808
Kacper Dudziak <kacper.dudziak@elympics.ai>
2025-07-29 12:22:47 +0200

    chore: simplify namespaces in references

--- Runtime/Scripts/ExternalCommunicators/Ui/IExternalUiCommunicator.cs
+++ Runtime/Scripts/ExternalCommunicators/Ui/IExternalUiCommunicator.cs
@@ -17,11 +17,11 @@ namespace ElympicsPlayPad.ExternalCommunicators.Ui
         /// Display a list of all active tournaments the local player has participated in.
         /// This list includes the daily tournament, tournaments created by the local player and ones the player has completed at least one match in.
         /// If the player selects one of the tournaments in the displayed list, it will be set as the current active tournament and
-        /// <see cref="ElympicsPlayPad.ExternalCommunicators.Tournament.IExternalTournamentCommunicator.TournamentUpdated"/> event will be raised.
+        /// <see cref="Tournament.IExternalTournamentCommunicator.TournamentUpdated"/> event will be raised.
         /// </summary>
         /// <remarks>
         /// If you want to set a tournament not contained in the list displayed by this method as the current tournament,
-        /// you can use <see cref="ElympicsPlayPad.ExternalCommunicators.Tournament.IExternalTournamentCommunicator.SetActiveTournament"/>.
+        /// you can use <see cref="Tournament.IExternalTournamentCommunicator.SetActiveTournament"/>.
         /// </remarks>
         UniTask DisplayTournamentsListing() => Display(TournamentsListing);
 
