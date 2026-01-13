829e2e7dea08f0e727bdc156cc8a382442dc506c
Krzysztof Pięta <krzysztof.pieta@elympics.cc>
2026-01-13 14:00:10 +0100

    chore: package update

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyCompany("Elympics")]
 [assembly: AssemblyProduct("Elympics PlayPad")]
-[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
 [assembly: InternalsVisibleTo("ElympicsPlayPad.Tests")]
 [assembly: InternalsVisibleTo("ElympicsPlayPad.Editor")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
